606975a484ed2b49277681fd302b16358c96e1f5
VIPULSHARMA <vipulsharma5002@gmail.com>
2025-04-12 00:07:50 +0530

    fix: added STATIC_ROOT for Render deployment

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
