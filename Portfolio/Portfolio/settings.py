0ea54e0f399a70cb3ab6d9f27cfe7935293cc484
VIPULSHARMA <vipulsharma5002@gmail.com>
2025-04-12 01:08:17 +0530

    Fixed staticfiles path for Render

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'Portfolio' / 'static']
+STATICFILES_DIRS = [os.path.join(os.path.dirname(BASE_DIR), 'Portfolio', 'static')]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles' 
 # This production code might break development mode, so we check whether we're in DEBUG mode
