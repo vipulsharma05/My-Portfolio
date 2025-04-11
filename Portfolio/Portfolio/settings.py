0c1a28e7f7623bc6f9db785602adbb565ce692a7
VIPULSHARMA <vipulsharma5002@gmail.com>
2025-04-12 01:04:50 +0530

    Added static files (images, css, js)

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'Portfolio' / 'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
